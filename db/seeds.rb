f63bce468d7c19be869d9f76b2f2723c48f1f8f5
Stefano Semeraro <semeraro.stefano@hotmail.it>
2015-09-16 12:57:39 +0000

    seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,35 @@
 Category.create!([
-  {name: "Computer"},
-  {name: "Cellulari"}
+  {name: "Accessori"},
+  {name: "Desktop"},
+  {name: "Notebook"},
+  {name: "Tablet"}
+])
+Product.create!([
+  {name: "Lenovo C50-30", desc: "Microfono incorporato, Full HD, Lettore di schede intetgrato, Built-in camera", image: "image/upload/v1442408108/rrzvvxsakdzrewrlftj9.jpg", price: "529.0", discount: 15, category_id: 3}
 ])
 Store.create!([
   {place: "Campus Universitario Ecotekne, Monteroni"},
-  {place: "Piazza Sant'Oronzo, Lecce"},
-  {place: "Thespace cinema, Surbo"}  
+  {place: "Elettronica Sud, Lecce"},
+  {place: "Trony, Lecce"},
+  {place: "Euronics, Lecce"},
+  {place: "Elettronica componenti SRL, Brindisi"},
+  {place: "Mediaworld, Surbo"},
+  {place: "Marcopolo, Cavallino"}
 ])
 User.create!([
   {name: "admin", admin: true, password_digest: "$2a$10$rc/FhA9cYNI6xj7UqcDWduUAj5BsCkorhtuMOtGSxnUlnqOsXlk6W"},
-  {name: "user", admin: false, password_digest: "$2a$10$cfpRfjlWzJnUzAVND6WD7e4k8Vb1kKt7iFwsW7oTf8C6kOntnO/xK"},
   {name: "edipo barbieri", admin: false, password_digest: "$2a$10$zUugbd3NPxv7.hPgH8KlQ.iNDKvdhoXdyNP0nmSOoGPOU.6cr0uYa"},
   {name: "ilario giordano", admin: false, password_digest: "$2a$10$J1Oy6j.m.kcPYoJHxyjY5u9c2q2YkYpkPaM3GTo//4Knd/XPk7MoW"},
   {name: "lino martino", admin: false, password_digest: "$2a$10$ICeWWyue.2USFV65I67KiedVvYL.zwjRBzER3hrXFxLdMx/cipM8i"},
   {name: "osvaldo fontana", admin: false, password_digest: "$2a$10$fsUrdAu6WP.gMQBNcY9YtOoRjy4C9cZeCviEnPtLMc4NONUP9BKYe"},
+  {name: "user", admin: false, password_digest: "$2a$10$cfpRfjlWzJnUzAVND6WD7e4k8Vb1kKt7iFwsW7oTf8C6kOntnO/xK"},
   {name: "vinicio de angelis", admin: false, password_digest: "$2a$10$alUS0HOhUVvGNE324P0rJ.nRwsrtdN3kxoARiiTkKo31e.y/DeX7O"}
+])
+Product::HABTM_Stores.create!([
+  {product_id: 1, store_id: 6},
+  {product_id: 1, store_id: 8}
+])
+Store::HABTM_Products.create!([
+  {product_id: 1, store_id: 6},
+  {product_id: 1, store_id: 8}
 ])
